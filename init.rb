90ab4837505010f40916be37c5895d4b6da5668b
darioo <sb.kowalwawa+github@gmail.com>
2014-07-12 22:00:11 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require 'redmine'
 
-require_dependency 'author_notification_emails/patches/mailer_patch'
+require_dependency 'improved_subtasks/patches/issue_patch'
 
-Redmine::Plugin.register :redmine_author_notification_emails do
+Redmine::Plugin.register :redmine_improved_subtasks do
   
-  name 'Redmine Author Notification Emails'
+  name 'Redmine Improved ParentTask behavior in subtasks structure'
   author 'Dariusz Kowalski'
-  description 'This plugin change FROM adres at all Emails to Logged User email'
-  version '1.0.0' 
+  description 'This plugin change default behavior of priorities, start and due date at parent tasks in sub-tasks structure'
+  version '0.0.1' 
   author_url 'https://github.com/darioo'
-  url "https://github.com/darioo/redmine_author_notification_emails" if respond_to?(:url)
+  url "https://github.com/darioo/redmine_improved_subtasks" if respond_to?(:url)
 end
